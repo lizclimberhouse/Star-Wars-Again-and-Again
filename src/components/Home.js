4b41c56c7a0f83098889aeba2a1fc50b43b4a349
Liz House <lizehouse@gmail.com>
2018-04-25 23:55:22 -0600

    list of names

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Container } from 'semantic-ui-react';
+import axios from 'axios';
 
 class Home extends Component {
+  state = { people: {} }
+
+  componentDidMount() {
+    axios.get('https://swapi.co/api/people/')
+      .then( res => {
+        this.setState({ people: res.data.results })
+        // console.log(this.state.people)
+      })
+      .catch(err => {
+        // console.log(err.response)
+      })
+  }
+
   render() {
+    const { people } = this.state;
+    // { people[0] ? console.log(people[0].name) : null }
     return (
-      <Header as='h1' textAlign='center'>Home Component</Header>
+      <Container>
+        <Header as='h1' textAlign='center'>Home Component</Header>
+        { people[0] ?
+        people.map((character, i) => {
+          return(
+          <p key={i}>{character.name}</p>
+          )
+        })
+        :
+        null
+        }
+      </Container>
     );
   }
 }
 
 export default Home;
+
