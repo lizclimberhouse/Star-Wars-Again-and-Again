fa34f522d42325dabf29c02ec3d30c1bb3ea4341
Liz House <lizehouse@gmail.com>
2018-05-17 18:18:44 -0600

    notes for other method

--- src/components/Films.js
+++ src/components/Films.js
@@ -10,6 +10,7 @@ class Films extends React.Component {
     axios.get('https://swapi.co/api/films/')
       .then( res => {
         this.setState({ films: res.data.results })
+        console.log(this.state.films)
       })
       .catch(err => {
         // console.log(err.response)
@@ -23,6 +24,21 @@ class Films extends React.Component {
         <Header as='h1' textAlign='center'>Films Component</Header>
         { films[0] ?
         films.map((film) => {
+
+        // START this allows me to find the correct film id for the actual APi call, but I don't know how to save it to use in the return Link.
+          const urlEach = film.url.split("")
+          urlEach.map((item, film) => {
+            if (item > 0) {
+              const id = item
+              console.log(id)
+              // this.showLink(id, film)
+              // if I put the return and Link in this separate function nothing displays.
+            }
+            else
+              null
+          })
+          //END
+          
           return(
           <Link key={film.title} to={`/film/${film.title}`}>{film.title}<br /></Link>
           )
